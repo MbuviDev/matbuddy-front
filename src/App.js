4eaf87a66d995a2b685c1fcc9cd56691a073800d
Meshack <mbuvi.meshack99@gmail.com>
2023-06-19 17:44:53 +0300

    added navbar and navitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Form } from './Components/Form';
 import './App.css';
 
-// import { ExpensesForm } from './Components/Home'
+
 
 function App() {
   return (
     <div className="bg-gray-200 h-screen">
-      {/* <ExpensesForm /> */}
+    
       <Form />
     </div>
   );
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar () {
+
+    return (
+
+        <div className={`w-9 md:w-12 h-screen overflow-hidden bg-blue-700 pt-16 flex flex-col sticky top-0
+        transition-all duration-200 hover:w-40 md:hover:w-60`}>
+
+            <div className="flex-grow">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
