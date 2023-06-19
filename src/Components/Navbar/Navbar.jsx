1c168082ccd1f9bd13e62d40e11ac09e687ef2e4
Meshack <mbuvi.meshack99@gmail.com>
2023-06-19 18:04:35 +0300

    updated Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import NavItem from "./NavItem";
+import dashboard from "./Assets/dashboard.png";
+import loading from "./Assets/loading.png"
+import station from './Assets/station.png';
+import expenses from './Assets/expenses.png';
+import exit from './Assets/exit.png';
+
 
 function Navbar () {
 
@@ -8,7 +15,14 @@ function Navbar () {
         transition-all duration-200 hover:w-40 md:hover:w-60`}>
 
             <div className="flex-grow">
+                <NavItem image={dashboard} path="/dashboard" text="Dashboard"/>
+                <NavItem image={loading} path="/loading" text="Loading"/>
+                <NavItem image={expenses} path="expenses" text="Expenses" flex="1 0 auto"/>
+                <NavItem image={station} path="/station" text="Station"/>
+            </div>
 
+            <div className="mb-16"> 
+                <NavItem image={exit} path="/" text="Logout" flex="0 0 auto"/>
             </div>
 
         </div>
