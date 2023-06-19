dc5590a29a68c49c6a40f2b56849d912c2974bfa
Meshack <mbuvi.meshack99@gmail.com>
2023-06-19 18:09:57 +0300

    updated navitem

--- src/Components/Navbar/NavItem.jsx
+++ src/Components/Navbar/NavItem.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
-function NavItem () {
+function NavItem ({image , text, path}) {
     const nav = useNavigate();
 
     return (
-        <div>
+        <div
+        onClick={()=>(path)}
+        className="flex items-center cursor-pointer py-3">
+
+    <img src={image} className="mr-2 px-2 md:px-3  w-9 md:w-12" alt={text}/> 
+    <span className="align-middle hover:text-white transition-colors ">{text}</span>          
 
 
         </div>
