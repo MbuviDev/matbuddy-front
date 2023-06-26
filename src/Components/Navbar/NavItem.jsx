7818a56d2481bdfab200f66f15c0162b0525e3e1
Meshack <mbuvi.meshack99@gmail.com>
2023-06-26 10:59:06 +0300

    Merged branch

--- src/Components/Navbar/NavItem.jsx
+++ src/Components/Navbar/NavItem.jsx
@@ -6,17 +6,16 @@ import "./styles.css"
 function NavItem ({image , text, path}) {
     const nav = useNavigate();
 
-    return (
-        <div
-        onClick={()=>nav(path)}
+  return (
+    <div 
+    onClick={()=>nav(path)}
         className="flex items-center cursor-pointer py-3">
 
     <img src={image} className="mr-2 px-2 md:px-3  w-9 md:w-12" alt={text}/> 
-    <span className="align-middle hover:text-white transition-colors ">{text}</span>          
+    <span className="align-middle hover:text-white transition-colors ">{text}</span>
 
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default NavItem
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,22 +9,22 @@ import exit from './Assets/exit.png';
 
 function Navbar () {
 
-    return (
+  return (
 
         <div className={`w-9 md:w-12 h-screen overflow-hidden bg-blue-700 pt-16 flex flex-col sticky top-0
         transition-all duration-200 hover:w-40 md:hover:w-60`}>
 
-            <div className="flex-grow">
+      <div className="flex-grow">
                 <NavItem image={dashboard} path="/dashboard" text="Dashboard"/>
                 <NavItem image={loading} path="/loading" text="Loading"/>
                 <NavItem image={expenses} path="expenses" text="Expenses" flex="1 0 auto"/>
                 <NavItem image={station} path="/station" text="Station"/>
-            </div>
-
-            <div className="mb-16"> 
+      </div>
+      
+      <div className="mb-16">
                 <NavItem image={exit} path="/" text="Logout" flex="0 0 auto"/>
-            </div>
-        </div>
+      </div>
+    </div>
     )
 }
 
